f09f0d0f71e57d06d851b1fe92b87aba030a25fb
Nguyen Van Nguyen <nguyenvannguyen192402@gmail.com>
2024-08-19 13:06:04 +0700

    First commit - Add Maven project

--- src/main/java/com/user/servlet/RegisterServlet.java
+++ src/main/java/com/user/servlet/RegisterServlet.java
@@ -15,14 +15,6 @@ import com.entity.User;
 @SuppressWarnings("serial")
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet{
-	
-	
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
-	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
