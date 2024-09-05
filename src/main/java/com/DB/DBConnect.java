686523f76e04e9000a9144e090d3fa7a481b03e5
Nguyen Van Nguyen <134776169+Nguyen-Van-Nguyen-github@users.noreply.github.com>
2024-09-05 22:08:58 +0700

    Update DBConnect.java

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -8,7 +8,7 @@ public class DBConnect {
 	public static Connection getConn() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/ebook-app", "root", "nguyen");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/ebook-app", "username", "password");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
